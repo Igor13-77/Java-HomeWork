01956ad5aa82aac964c499aba1a871393dfa2f40
Igor13-77 <navrocki77@gmail.com>
2023-06-28 15:08:52 +0200

    Task3

--- Homework002/task1.java
+++ Homework002/task1.java
@@ -1,7 +1,7 @@
-// Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, 
-// используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
-// Если значение null, то параметр не должен попадать в запрос.
-// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+/** Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, 
+используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
+Если значение null, то параметр не должен попадать в запрос.
+Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"} */
 
 public class task1 {
     public static void main(String[] args) throws Exception {
