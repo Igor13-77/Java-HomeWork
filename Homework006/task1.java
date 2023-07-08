193477fa9c9b5f40b25f6a3580312209d5267b9c
Igor13-77 <navrocki77@gmail.com>
2023-07-08 16:13:00 +0200

    Task

---
+++ Homework006/task1.java
@@ -0,0 +1,41 @@
+package Homework006;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/* 
+Set из котов
+*/
+
+    public class Solution {
+        public static void main(String[] args) {
+            Set<Cat> cats = createCats();
+            
+            Iterator<Cat> it = cats.iterator();
+            if(it.hasNext())
+                cats.remove(it.next());
+            printCats(cats);
+        }
+
+        public static Set<Cat> createCats() {
+            
+            Set<Cat> cats = new HashSet<Cat>();
+            cats.add(new Cat());
+            cats.add(new Cat());
+            cats.add(new Cat());
+            return cats;
+        }
+
+        public static void printCats(Set<Cat> cats) {
+            
+            for(int i = 0; i < cats.size(); i++){
+                System.out.println(cats);
+            }
+        }
+
+        
+        public static class Cat{
+
+        }
+    }
